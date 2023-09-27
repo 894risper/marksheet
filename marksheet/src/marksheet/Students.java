597bd0b9131d5a72f0f23f62ecb0593601111571
894risper <145770911+894risper@users.noreply.github.com>
2023-09-27 18:33:47 +0300

    Add files via upload

---
+++ marksheet/src/marksheet/Students.java
@@ -0,0 +1,108 @@
+package marksheet;
+
+import java.util.Scanner;
+
+public class Students {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	int i=1;
+	do {
+		
+		
+	int a;
+		Scanner in=new Scanner (System.in);
+		System.out.print("enter sNO ");
+		a=in.nextInt();
+		String b;
+		Scanner input=new Scanner (System.in);
+		System.out.print("Enter students name ");
+		b=input.next();
+		int c;
+		Scanner inn=new Scanner (System.in);
+		System.out.print("Enter Sub1: ");
+		c=inn.nextInt();
+		
+		int d;
+		Scanner inu=new Scanner (System.in);
+		System.out.print("Enter Sub2: ");
+		d=inu.nextInt();
+		
+		int e;
+		Scanner iny=new Scanner (System.in);
+		System.out.print("Enter Sub3: ");
+		e=iny.nextInt();
+		
+		
+		int f;
+		Scanner inc=new Scanner (System.in);
+		System.out.print("Enter Sub4: ");
+		f=inc.nextInt();
+		
+		
+		int g;
+		Scanner ind=new Scanner (System.in);
+		System.out.print("Enter Sub5: ");
+		g=ind.nextInt();
+		
+		
+		int h;
+		Scanner inl=new Scanner (System.in);
+		System.out.print("Enter Sub6: ");
+		h=inl.nextInt();
+		i++;}
+	while (i<4);
+
+	
+	
+	System.out.println("Welcome to students mark list application");
+	System.out.println("SNo  Student name        Sub1    SUB2    SUB3    SUB4     SUB5   SUB6");
+	 int j;
+	 do {
+		 
+	 int a;
+	 String b;
+	 int c;
+	 int d;
+	 int f;
+	 int g;
+	 int h;
+	 
+	
+	System.out.print(""+a);
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+			
+		
+		
+		
+		
+
+		
+		
+		
+		
+		
+
+	}
+
+}
